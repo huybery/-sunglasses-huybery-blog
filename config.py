c3d37478bceb22659515e9c2db004e7f4743fb2e
huybery <707146450@qq.com>
2016-03-17 21:25:40 +0800

    add some config

--- config.py
+++ config.py
@@ -1,2 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'meiyoushezhimima'
+    SQLALCHEMY_COMMIT_ON_REARDOWN = True
+    SQLALCHEMY_DATABASE_URI=''
+    DEBUG = True
+
+    @staticmethod
+    def init_app(app):
+        pass
