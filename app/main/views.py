3f08339316cb5449a77d0f3545bab307a7a57be1
huybery <707146450@qq.com>
2016-03-18 23:33:27 +0800

    test the data.sqlite

--- app/main/views.py
+++ app/main/views.py
@@ -1,2 +1,22 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+from datetime import datetime
+from flask import  render_template,session,redirect,url_for
+
+from . import main
+from .forms import NameForm
+# from .. import db
+# from ..models import User
+
+@main.route('/', methods=['GET','POST'])
+def index():
+    form = NameForm()
+    if form.validate_on_submit():
+        session['name'] = form.name.data
+        return redirect(url_for('.index'))
+    return render_template('hello.html',
+                           form=form,
+
+                           know=session.get('know',False),
+                           current_time=datetime.utcnow()),200
--- config.py
+++ config.py
@@ -6,9 +6,18 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'meiyoushezhimima'
     SQLALCHEMY_COMMIT_ON_REARDOWN = True
-    SQLALCHEMY_DATABASE_URI=''
-    DEBUG = True
+    # SQLALCHEMY_DATABASE_URI=''
+
 
     @staticmethod
     def init_app(app):
         pass
+
+class ProductionConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+
+config = {
+    'production':ProductionConfig,
+    'default':ProductionConfig
+}
