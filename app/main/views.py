4fbf2b492b786987bed3172287d325c2c4aff55b
huybery <707146450@qq.com>
2016-03-23 00:49:21 +0800

    better login

--- app/main/views.py
+++ app/main/views.py
@@ -16,7 +16,7 @@ def index():
     if form.validate_on_submit():
         session['name'] = form.name.data
         return redirect(url_for('.index'))
-    return render_template('base.html',
+    return render_template('index.html',
                            form=form,
                            know=session.get('know',False),
                            current_time=datetime.utcnow()),200
