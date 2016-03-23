f0aff7120c97e5020b488d0c74a82c337ef625d7
huybery <707146450@qq.com>
2016-03-24 00:15:15 +0800

    change log

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # huybery-blog
-:neckbeard: welcome fork or start(2016.3.16
+:neckbeard: welcome fork or start (2016.3.16
 
-:bear: 开发进度有点慢，sad(2016.3.18
+:bear: 开发进度有点慢，sad (2016.3.18
 
- :yum: 增加了登陆功能 （2016.3.23
+:yum: 增加了登陆功能 （2016.3.23
+
+:alien: 增加了文章发布(Markdown)，单文章页路由 （2016.3.24
--- app/models.py
+++ app/models.py
@@ -54,10 +54,17 @@ class Post(db.Model):
 
     @staticmethod
     def on_change_body(target, value, oldvalue, initiator):
-        allowed_tags=['a', 'abbr', 'acronym', 'b', 'blockquote', 'code', 'em', 'i', 'li', 'ol', 'pre', 'strong', 'ul', 'h1', 'h2', 'h3', 'p']
+        allowed_tags=['a', 'abbr', 'acronym', 'b',
+                      'blockquote', 'code', 'em',
+                      'i', 'li', 'ol', 'pre',
+                      'strong', 'ul', 'h1',
+                      'h2', 'h3', 'p', 'img']
+        allowed_attrs = {'img': ['src', 'alt']}
         target.body_html = bleach.linkify(bleach.clean(
-            markdown(value, output_format = 'html'),
-            tags=allowed_tags,strip=True
+            markdown(value, output_format='html'),
+            tags=allowed_tags,
+            attributes=allowed_attrs,
+            strip=True
         ))
 
-db.event.listen(Post.body,'set',Post.on_change_body)
+db.event.listen(Post.body, 'set', Post.on_change_body)
