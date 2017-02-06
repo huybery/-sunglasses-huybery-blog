c05c1a08f8b540714155f5b951f90bd3d7337688
huybery <707146450@qq.com>
2017-02-06 10:30:03 +0800

    [Refactor] 加入用户密码认证

--- app/models.py
+++ app/models.py
@@ -1,10 +1,12 @@
 from flask_sqlalchemy import SQLAlchemy
 from passlib.apps import custom_app_context
+from datetime import datetime
 
 db = SQLAlchemy()
 
 
 class User(db.Model):
+    __tablename__ = "user"
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True)
     password_hash = db.Column(db.String(128))
@@ -22,3 +24,25 @@ class User(db.Model):
         :return: verify password equal old password
         """
         return custom_app_context.verify(password, self.password_hash)
+
+
+class Post(db.Model):
+    __tablename__ = "posts"
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), unique=True)
+    body = db.Column(db.Text)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    like = db.Column(db.Integer)
+    tag_id = db.Column(db.Integer, db.ForeignKey('tags.id'))
+
+
+class Tag(db.Model):
+    __tablename__ = "tags"
+    id = db.Column(db.Integer, primary_key=True)
+    tag = db.Column(db.String(64))
+    posts = db.relationship("Post", backref="tag")
+
+
+class View(db.Model):
+    __tablename__ = "views"
+    view_count = db.Column(db.Integer, primary_key=True)
--- app/views.py
+++ app/views.py
@@ -1,13 +1,16 @@
-from flask import request, abort, jsonify, url_for, Blueprint
+from flask import request, abort, jsonify, Blueprint, g
+from flask_httpauth import HTTPBasicAuth
 from .models import db, User
+
+auth = HTTPBasicAuth()
 
 api = Blueprint('api', __name__)
 
 
 @api.route('/api/users', methods=['POST'])
 def get_user():
-    username = request.json.get('username')
-    password = request.json.get('password')
+    username = request.json.get('userName')
+    password = request.json.get('passWord')
     if username is None or password is None:
         abort(400)
     if User.query.filter_by(username=username).first() is not None:
@@ -17,3 +20,18 @@ def get_user():
     db.session.add(user)
     db.session.commit()
     return jsonify({'username': user.username}), 201
+
+
+@auth.verify_password
+def verify_password(username, password):
+    user = User.query.filter_by(username=username).first()
+    if not user or not user.verify_password(password):
+        return False
+    g.user = user
+    return True
+
+
+@api.route('/api/admin')
+@auth.login_required
+def admin():
+    return jsonify({'data': 'Hello, %s' % g.user.username})
