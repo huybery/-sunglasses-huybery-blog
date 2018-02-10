e61b60060a3db6bf25fac7ec7b4a0b0a0f28ceea
huybery <707146450@qq.com>
2018-02-10 18:53:30 +0800

    [update] api comment

--- backend/app/api.py
+++ backend/app/api.py
@@ -63,13 +63,17 @@ def verify_password(username_or_token, password):
 	g.user = user
 	return True
 
+@blog.route("/api/token")
+@auth.login_required
+def get_auth_token():
+	"""
+	获取 token
+	"""
+	token = g.user.generate_auth_token()
+	return jsonify({'token': token.decode('ascii')})
+
 @blog.route("/api/resource")
 @auth.login_required
 def get_resouce():
 	return jsonify({'data': 'Hello, %s' % g.user.username})
 
-@blog.route("/api/token")
-@auth.login_required
-def get_auth_token():
-	token = g.user.generate_auth_token()
-	return jsonify({'token': token.decode('ascii')})
