cf1c6a7e4ff4e3c4ec4cd2065fc8c7e2ba986d95
huybery <707146450@qq.com>
2018-02-20 00:08:01 +0800

    [add] can edit table component

--- frontend/src/components/dashboard/User.vue
+++ frontend/src/components/dashboard/User.vue
@@ -3,110 +3,84 @@
     <Row type="flex" justify="center" class="user-list">
         <Col :xs="20" :sm="16" :md="12" :lg="18">
             <Card>
-                <p slot="title">用户列表</p>
-                <Table border :columns="columns" :data="data"></Table>
+                <p slot="title">
+                    <Icon type="ios-list-outline"></Icon>
+                        用户列表
+                </p>
+                    <div class="edittable-table-height-con">
+                        <can-edit-table v-model="editInlineData" :columns-list="editInlineColumns">
+                        </can-edit-table>
+                    </div>
             </Card>
         </Col>
     </Row>
 </template>
 
 <script>
+import canEditTable from './canEditTable.vue'
 export default {
+  components: {
+    canEditTable
+  },
   data () {
     return {
-      columns: [
-        {
-          title: 'Name',
-          key: 'name',
-          render: (h, params) => {
-            return h('div', [
-              h('Icon', {
-                props: {
-                  type: 'person'
-                }
-              }),
-              h('strong', params.row.name)
-            ])
-          }
-        },
-        {
-          title: 'Age',
-          key: 'age'
-        },
-        {
-          title: 'Address',
-          key: 'address'
-        },
-        {
-          title: 'Action',
-          key: 'action',
-          width: 150,
-          align: 'center',
-          render: (h, params) => {
-            return h('div', [
-              h('Button', {
-                props: {
-                  type: 'primary',
-                  size: 'small'
-                },
-                style: {
-                  marginRight: '5px'
-                },
-                on: {
-                  click: () => {
-                    this.show(params.index)
-                  }
-                }
-              }, 'View'),
-              h('Button', {
-                props: {
-                  type: 'error',
-                  size: 'small'
-                },
-                on: {
-                  click: () => {
-                    this.remove(params.index)
-                  }
-                }
-              }, 'Delete')
-            ])
-          }
-        }
-      ],
-      data: [
-        {
-          name: 'John Brown',
-          age: 18,
-          address: 'New York No. 1 Lake Park'
-        },
-        {
-          name: 'Jim Green',
-          age: 24,
-          address: 'London No. 1 Lake Park'
-        },
-        {
-          name: 'Joe Black',
-          age: 30,
-          address: 'Sydney No. 1 Lake Park'
-        },
-        {
-          name: 'Jon Snow',
-          age: 26,
-          address: 'Ottawa No. 2 Lake Park'
-        }
-      ]
+      editInlineData: [],
+      eiditInlineColumns: []
     }
   },
   methods: {
-    show (index) {
-      this.$Modal.info({
-        title: 'User Info',
-        content: `Name：${this.data[index].name}<br>Age：${this.data[index].age}<br>Address：${this.data[index].address}`
-      })
+    getData () {
+      const editInlineAndCellData = [
+        {
+          name: 'Aresn',
+          //   sex: '男',
+          work: '前端开发'
+        },
+        {
+          name: 'Lison',
+          //   sex: '男',
+          work: '前端开发'
+        },
+        {
+          name: 'lisa',
+          //   sex: '女',
+          work: '程序员鼓励师'
+        }
+      ]
+      const editInlineColumns = [
+        {
+          title: '序号',
+          type: 'index',
+          align: 'center'
+        },
+        {
+          title: '用户名',
+          align: 'center',
+          key: 'name',
+          editable: true
+        },
+        {
+          title: '岗位',
+          align: 'center',
+          key: 'work',
+          editable: true
+        },
+        {
+          title: '操作',
+          align: 'center',
+          key: 'handle',
+          handle: ['edit', 'delete']
+        }
+      ]
+      return [editInlineAndCellData, editInlineColumns]
     },
-    remove (index) {
-      this.data.splice(index, 1)
+    setData () {
+      this.editInlineData = this.getData()[0]
+      this.editInlineColumns = this.getData()[1]
     }
+  },
+  created () {
+    this.setData()
   }
 }
 </script>
